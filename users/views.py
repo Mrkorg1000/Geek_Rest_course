46268ba4d254c7a9a9c95ac4cd3a760f2df183c8
Mrkorg1000 <korg1000@yandex.ru>
2021-12-27 01:49:47 +0300

    Code style fix. Lesson_1

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework.viewsets import ModelViewSet
+
 from .models import User
 from .serializers import UserModelSerializer
 
